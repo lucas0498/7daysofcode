4aba9e6973b8afd9e607fa0f4c2a7843849aa5b7
Lucas Gonçalves <lucass.goncalves@outlook.com>
2023-01-03 12:55:11 -0300

    Criando função de remover produto da lista

--- 7daysofcode-5/main.js
+++ 7daysofcode-5/main.js
@@ -2,37 +2,90 @@ let hortifruti = [];
 let congelados = [];
 let laticinios = [];
 let doces = [];
-let adicionarMais = '';
+let removeProd = '';
+let produto = '';
+let indexProd;
 
-while(adicionarMais != 'não'){
-    adicionarMais = prompt('Deseja adicionar algo à sua lista de compras?(sim ou não)')
-    if (adicionarMais == 'sim'){
+let alterarLista = ''; 
+
+
+
+while(alterarLista != 'não'){
+
+    validaPergunta();
     
-        let produto = prompt('O que?')
-        let categoria = prompt('Em qual categoria isso se encaixa? Responda 1 para HORTIFRUTI, 2 para CONGELADOS, 3 para LATICINIOS e 4 para DOCES')
+    if (alterarLista == 'sim'){
+    
+        produto = prompt('O que?');
+        let categoria = prompt('Em qual categoria isso se encaixa? Responda 1 para HORTIFRUTI, 2 para CONGELADOS, 3 para LATICINIOS e 4 para DOCES');
     
     if(categoria == 1){
-        hortifruti.push(produto)
+        adicionarProduto(hortifruti)
     }
     else if(categoria == 2){
-        congelados.push(produto)
+        adicionarProduto(congelados)
     }
     else if(categoria == 3){
-        laticinios.push(produto)
+        adicionarProduto(laticinios)
     }
     else if(categoria == 4){
-        doces.push(produto)
+        adicionarProduto(doces)
     } else{
-        alert('Categoria Inválida.')
+        alert('Categoria Inválida.');
     }
 
-}else if(adicionarMais == 'não'){
+}else if(alterarLista == 'não'){
     alert(`Lista de compras: \n 
     Hortifruti: ${hortifruti} \n
     Congelados: ${congelados} \n
     Laticinios: ${laticinios} \n
-    Doces: ${doces} \n`)
+    Doces: ${doces} \n`);
+}
+else if (alterarLista == 'remover'){
+    categoria = prompt('Deseja remover um item de qual categoria? Responda 1 para HORTIFRUTI, 2 para CONGELADOS, 3 para LATICINIOS e 4 para DOCES');
+    if(categoria == 1){
+        removerProduto(hortifruti);
+    }
+    else if(categoria == 2){
+        removerProduto(congelados);
+    }
+    else if(categoria == 3){
+        removerProduto(laticinios);
+    }
+    else if(categoria == 4){
+        removerProduto(doces);
+    }else{
+        alert('Categoria Inválida.');
+    }
 }
 }
+function validaPergunta(){
+    if(hortifruti.length > 0 || congelados.length > 0 || laticinios.length > 0 || doces.length > 0){
+        alterarLista = prompt(`Deseja adicionar algo à sua lista de compras? (sim ou não) \n
+    Caso queira remover algo, digite remover.`);
+    } else{
+        alterarLista = prompt('Deseja adicionar algo à sua lista de compras? (sim ou não)');
+    }
+}
+
+function adicionarProduto(array){
+    array.push(produto);
+}
+
+
+function removerProduto(array){
+    alert(array);
+        removeProd = prompt("Qual produto deseja remover?");
+        array.includes(removeProd);
+            indexProd = array.indexOf(removeProd);
+            if(indexProd >= 0){
+                array.splice(indexProd, 1);
+                alert(`Produto ${removeProd} foi removido com sucesso! \n
+                ${array}`);
+            }else{
+                alert('Produto não encontrado!');
+            }           
+        
+        } 
 
 
