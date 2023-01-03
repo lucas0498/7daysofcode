a54b6512b97db0483952da929bbb26ed4109fb0b
Lucas Gonçalves <lucass.goncalves@outlook.com>
2023-01-03 11:16:53 -0300

    Criando o arquivo main js

---
+++ 7daysofcode-5/main.js
@@ -0,0 +1,38 @@
+let hortifruti = [];
+let congelados = [];
+let laticinios = [];
+let doces = [];
+let pergunta = '';
+
+while(pergunta != 'não'){
+    pergunta = prompt('Deseja adicionar algo à sua lista de compras?(sim ou não)')
+    if (pergunta == 'sim'){
+    
+        let produto = prompt('O que?')
+        let categoria = prompt('Em qual categoria isso se encaixa? Responda 1 para HORTIFRUTI, 2 para CONGELADOS, 3 para LATICINIOS e 4 para DOCES')
+    
+    if(categoria == 1){
+        hortifruti.push(produto)
+    }
+    else if(categoria == 2){
+        congelados.push(produto)
+    }
+    else if(categoria == 3){
+        laticinios.push(produto)
+    }
+    else if(categoria == 4){
+        doces.push(produto)
+    } else{
+        alert('Categoria Inválida.')
+    }
+
+}else if(pergunta == 'não'){
+    alert(`Lista de compras: \n 
+    Hortifruti: ${hortifruti} \n
+    Congelados: ${congelados} \n
+    Laticinios: ${laticinios} \n
+    Doces: ${doces} \n`)
+}
+}
+
+
